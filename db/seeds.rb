ab89bb52ca67b8f1d71cc3c30143e2b3e17858a1
Etienne <etienne.burdet@gmail.com>
2019-11-14 10:56:28 +0100

    basic index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cosy lof in Paris',
+  address: '1 Boulevard de de Reuilly 7012 Paris',
+  description: 'Dans une rotonde, superbe vue sur le boulevard',
+  price_per_night: 120,
+  number_of_guests: 6
+)
